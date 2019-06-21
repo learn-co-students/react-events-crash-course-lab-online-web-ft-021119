0d69c87776280ef504679bf4d91673a256111e5a
github username <github email address>
2019-06-21 13:16:31 -0400

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -21,7 +21,7 @@ export default class ChromeBoisDomain extends Component {
       <canvas
         onMouseMove={this.handleMouseMove}
         onClick={() => {toggleCycling()}}
-        onKeyDown={this.handleKeyDown}
+        onKeyPress={this.handleKeyDown}
         width='900'
         height='600'
         tabIndex="0">
